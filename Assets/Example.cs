9193e4bce73340383fc2851b812a3f76a5fd7278
sakusaku <sakuraplus@163.com>
2017-05-03 18:36:37 +0800

    upload 1

---
+++ Assets/Example.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Copyright (c) 2015 Allan Pichardo
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using UnityEngine;
+using System;
+
+public class Example : MonoBehaviour
+{
+	public  GameObject ggg;
+	float ii=0;
+	AudioSource sd;
+	void Start ()
+	{
+		sd=GetComponent<AudioSource>();
+		//Select the instance of AudioProcessor and pass a reference
+		//to this object
+		AudioProcessor processor = FindObjectOfType<AudioProcessor> ();
+		processor.onBeat.AddListener (onOnbeatDetected);
+		processor.onSpectrum.AddListener (onSpectrum);
+	}
+
+	//this event will be called every time a beat is detected.
+	//Change the threshold parameter in the inspector
+	//to adjust the sensitivity
+	void onOnbeatDetected ()
+	{
+		//Debug.Log ("Beat!!!");
+		ii = 5;
+		ggg.transform.localScale   = new Vector3 (5,5,5);
+	}
+	float playtime=0;
+	float pausetime=0;
+	float unpausetime=0;
+	void Update () {
+		
+		if(ii>1){
+			ii -= ii / 5;
+			ggg.transform.localScale    = new Vector3 (ii,ii,ii);
+		}
+		if(Input.GetKeyDown (KeyCode.A)){
+		//	sd.Stop();
+			Debug.Log(sd.timeSamples+"/stop//"+sd.time+"///"+Time.time );
+		}
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if (!sd.isPlaying) {
+				sd.Play ();
+			
+				playtime = Time.time;
+				Debug.Log (sd.timeSamples + "/Play//" + sd.time + "///" + Time.time);
+			}
+		}
+		if(Input.GetKeyDown (KeyCode.S  )){
+			sd.Pause  ();
+			pausetime = Time.time;
+			Debug.Log(sd.timeSamples+"/Pause//"+sd.time+"///"+Time.time );
+		}
+		if(Input.GetKeyDown (KeyCode.X   )){
+			unpausetime = Time.time;
+			sd.UnPause   ();
+			Debug.Log(sd.timeSamples+"/UnPause//"+sd.time+"///"+Time.time );
+		}
+		if(Input.GetKeyDown (KeyCode.Q )){
+			//sd.UnPause   ();
+			Debug.Log((sd.time+playtime+unpausetime-pausetime  )+"/UnPause//"+sd.time+"///"+Time.time );
+		}
+
+	}
+	//This event will be called every frame while music is playing
+	void onSpectrum (float[] spectrum)
+	{
+		//The spectrum is logarithmically averaged
+		//to 12 bands
+
+		for (int i = 0; i < spectrum.Length; ++i) {
+			Vector3 start = new Vector3 (i, 0, 0);
+			Vector3 end = new Vector3 (i, spectrum [i], 0);
+			Debug.DrawLine (start, end);
+		}
+	}
+}
---
+++ Assets/main.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MusicData
+{
+	public float playtime = 0;
+	public float Average = 0;
+}
+
+public class main : MonoBehaviour {
+	AudioSource _audio;
+	public AudioClip mmm;
+	public  GameObject ggg;
+
+	[Range (0,64)]
+	public int hhh;
+	[Range (0,64)]
+	public int lll;
+	public float  low;
+	public float  mid;
+	public float high;
+	ArrayList md=new ArrayList() ;
+	//MusicData mdarr=new ArrayList 
+	// Use this for initialization
+	void Start () {
+		_audio=GetComponent<AudioSource> ();
+		_audio.PlayOneShot(mmm);
+		Application.targetFrameRate = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.A)){
+			//	_audio.Stop();
+			Debug.Log(_audio.timeSamples+"/stop//"+_audio.time+"///"+Time.time );
+		}
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			if (!_audio.isPlaying) {
+				_audio.Play ();
+				_audio.pitch = 1.5f;
+
+				//playtime = Time.time;
+				//Debug.Log (_audio.timeSamples + "/Play//" + _audio.time + "///" + Time.time);
+			}
+		}
+		if (_audio.isPlaying) {
+			recordmusicdata ();
+		} else {
+			Debug.Log ("stop");
+		}
+		if(Input.GetKeyDown (KeyCode.S  )){
+			_audio.Pause  ();
+			//pausetime = Time.time;
+			//Debug.Log(_audio.timeSamples+"/Pause//"+_audio.time+"///"+Time.time );
+		}
+		if(Input.GetKeyDown (KeyCode.X   )){
+
+			Debug.Log (md.Count );
+
+			//unpausetime = Time.time;
+			//_audio.UnPause   ();
+			//Debug.Log(_audio.timeSamples+"/UnPause//"+_audio.time+"///"+Time.time );
+		}
+		if(Input.GetKeyDown (KeyCode.Q )){
+			float lastavg = 0;
+			string stt="";
+			for (int i = 0; i < md.Count; i++) {
+				//Debug.Log (md);
+				MusicData mmm=(MusicData )md[i];
+				stt+=mmm.Average +","+mmm.playtime +" **";
+
+				Debug.DrawRay (new Vector3 (i - 1, mmm.Average * 100, 0), new Vector3 (1, lastavg, 0), Color.blue);
+					lastavg=mmm.Average ;
+			}
+			Debug.Log (stt);
+			//for (int i = 0; i < md.Count; i++) {
+				
+			//}
+			//_audio.UnPause   ();
+			//Debug.Log((_audio.time+playtime+unpausetime-pausetime  )+"/UnPause//"+_audio.time+"///"+Time.time );
+		}
+
+
+	
+	}
+
+	void recordmusicdata()
+	{
+		float[] spectrum = new float[64];
+		//float[] spectrumlow = new float[64];
+		low = 0;
+		high = 0;
+		mid = 0;
+		float musicenergy = 0;
+
+		AudioListener.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+		//AudioListener.GetSpectrumData(spectrumlow, 0, FFTWindow.Rectangular);
+
+		for (int i = 1; i < spectrum.Length - 1; i++)
+		{
+			Debug.DrawLine(new Vector3(i - 1, 100*spectrum[i], 0), new Vector3(i, 100*spectrum[i + 1], 0), Color.red);
+			lll = Mathf.Min (lll, hhh);
+			hhh = Mathf.Max (lll, hhh);
+			if (i < lll) {
+				low += spectrum [i];
+			} else if (i > hhh) {
+				high += spectrum [i];
+			} else {
+				mid += spectrum [i];
+			}
+			//low= spectrum[i]*100000;
+			musicenergy += spectrum [i];
+		}
+		//low = musicenergy;
+		musicenergy /= spectrum.Length;
+		//high = musicenergy*100000;
+		MusicData _md = new MusicData ();
+		_md.Average = musicenergy;
+		_md.playtime = _audio.time;
+		md.Add (_md);
+		if (low > mid && low > high) {
+			ggg.transform.position = new Vector3 (0, 0, 0);
+		} else if (mid > low && mid > high) {
+			ggg.transform.position = new Vector3 (0, 50, 0);
+		} else {
+			ggg.transform.position=new Vector3 (0, 100, 0);
+		}
+
+	}
+
+}
